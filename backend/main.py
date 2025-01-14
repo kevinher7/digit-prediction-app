daff2b9835d91d79adc0c77a6e353ffc2ffd3d0c
Kevin <kevinhernem@gmail.com>
2025-01-15 03:06:11 +0900

    chore: remove comments

--- backend/main.py
+++ backend/main.py
@@ -6,27 +6,26 @@ MODEL_PATH = "./backend/model/digit-prediction.keras"
 
 app = Flask(__name__)
 
-model = tf.keras.models.load_model(MODEL_PATH)
-
-# Add errorhandlers
-# @app.errorhandler(404)
-# def resource_not_found(e):
-#     return jsonify(error=str(e)), 404
+try:
+    model = tf.keras.models.load_model(MODEL_PATH)
+    print(f"\n\nSuccesfully imported model from {MODEL_PATH}\n\n")
+except Exception as e:
+    raise RuntimeError(f"Failed to load model from {MODEL_PATH}: {e}")
 
 
 @app.post("/api/prediction")
 def prediction_post():
     pixel_list = request.get_json()["pixel_data"]
-    pixel_array = np.array(pixel_list)
-
-    # add an extra dimension to give it a "batch-like" shape
-    # (1, 784)
-    pixel_array = np.expand_dims(pixel_array, 0)
 
     if (len(pixel_array) != 784):
         return "Invalid Request"
 
-    prediction = model.predict()
+    pixel_array = np.array(pixel_list)
+
+    # add an extra dimension to give it
+    # a "batch-like" shape of (1, 784)
+    pixel_array = np.expand_dims(pixel_array, 0)
+    prediction = model.predict(pixel_array)
 
     return prediction[0].tolist()
 
