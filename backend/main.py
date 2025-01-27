fab99dd4afa5b3b7352f67cb823d4b581d5e6760
Kevin <kevinhernem@gmail.com>
2025-01-27 13:37:16 +0900

    feat: implement fetching a prediction and preprocessing

--- backend/main.py
+++ backend/main.py
@@ -1,10 +1,17 @@
 from flask import Flask, jsonify, request
+from flask_cors import CORS
 import tensorflow as tf
+import matplotlib.pyplot as plt
 import numpy as np
 
-MODEL_PATH = "./backend/model/digit-prediction.keras"
+MODEL_PATH = "./model/digit-prediction.keras"
+
+(x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data()
+
 
 app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 
 try:
     model = tf.keras.models.load_model(MODEL_PATH)
@@ -18,18 +25,42 @@ def prediction_post():
     try:
         pixel_list = request.get_json()["pixel_data"]
 
-        if (len(pixel_list) != 784):
+        if (len(pixel_list) != 560*560*4):
             raise ValueError(
-                "Request array is not the appropriate length (784)")
+                "Request array is not the appropriate length (560x560x4)")
 
-        pixel_array = np.array(pixel_list)
+        raw_pixel_array = np.array(pixel_list)
+        rgb_pixel_array = raw_pixel_array.reshape(313600, 4)
+
+        rgb_pixel_array = rgb_pixel_array[:, :-1]
+
+        pixel_array = rgb_pixel_array[:, 0]*0.299 + \
+            rgb_pixel_array[:, 1]*0.587 + rgb_pixel_array[:, 2]*0.114
+
+        # Make it a big quare
+        pixel_array = pixel_array.reshape(560, 560)
+        # Separate into 28*28 groups of pixels
+        pixel_array = pixel_array.reshape(28, 20, 28, 20)
+        # Average the color in each group
+        pixel_array = pixel_array.mean(axis=(1, 3))
+        # Flatten the result
+        pixel_array = pixel_array.flatten()
+
+        plt.imshow(pixel_array.reshape(28, 28), cmap='gray')
+        plt.axis('off')  # Turn off the axes for a clean image display
+        plt.savefig("output_image.png", bbox_inches='tight', pad_inches=0)
+
+        pixel_array = pixel_array / 255  # Normalize
 
         # add an extra dimension to give it
         # a "batch-like" shape of (1, 784)
         pixel_array = np.expand_dims(pixel_array, 0)
         prediction = model.predict(pixel_array)
 
-        return jsonify({"prediction": prediction[0].tolist()})
+        prediction_normalized = prediction / np.sum(prediction[0])
+
+        return jsonify({"prediction": int(np.argmax(prediction[0])),
+                        "probabilities": prediction_normalized.tolist()}), 200
 
     except Exception as e:
         return jsonify({"error": str(e)}), 400
